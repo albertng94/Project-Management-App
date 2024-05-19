fa6ba35b53d06e4e06c797f5e186b8672119326d
albertng94 <albertng94@gmail.com>
2024-05-19 20:10:13 +0200

    Create component "ProjectsSidebar" skeleton.
    Created the component "ProjectsSidebar" skeleton containing a header, a
    button which will in the future be used to open a section to gather
    info for the projects, and a div which will vontain the list of current
    projects.
    
    Gave the component some basic styling with TailWind CSS classes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import ProjectsSidebar from "./assets/components/ProjectsSidebar";
+
 function App() {
   return (
     <>
+      <ProjectsSidebar />
+      Main Display
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
     </>
   );
