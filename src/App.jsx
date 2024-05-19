c6de4b4caad0336fb4b69c83b628ce336f697fb0
albertng94 <albertng94@gmail.com>
2024-05-19 21:19:46 +0200

    Create "MainDisplay" component.
    Created a "MainDisplay" component with the logo img, a header and a
    paragraph which inform the user that no project is bein selected at the
    moment, and a button which can be used to create a new project (same
    function as the one in the "ProjectsSidebar" component).
    
    Gave it some basic styling with Tailwind CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import ProjectsSidebar from "./assets/components/ProjectsSidebar";
+import MainDisplay from "./assets/components/MainDisplay";
 
 function App() {
   return (
     <>
       <ProjectsSidebar />
-      Main Display
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <MainDisplay />
     </>
   );
 }
---
+++ src/assets/components/MainDisplay.jsx
@@ -0,0 +1,16 @@
+import logo from "../no-projects.png";
+
+export default function MainDisplay() {
+    return (
+        <div className="w-full pt-40 flex flex-col items-center gap-5">
+            <img 
+            src={logo} 
+            alt="A drawing of a notebook and a pen"
+            className="w-1/6 max-w-20" 
+            />
+            <h2 className="font-bold text-2xl text-stone-500">No Project Selected</h2>
+            <p className="text-lg text-stone-400">Select a project or get started with a new one</p>
+            <button className="m-5 py-2 px-5 bg-stone-700 text-lg text-stone-400 rounded-lg">Create new project</button>
+        </div>
+    );
+}
