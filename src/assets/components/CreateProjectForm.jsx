7589ce27e82c8f3749206c7d0cd7328894e0654d
albertng94 <albertng94@gmail.com>
2024-05-24 23:45:57 +0200

    Correct "CreateProjectForm" style and delete portal.
    The "CreateProjectForm" component was not correctly positioned in the
    display and the fundamental cause for this was that it was being
    forwarded via portal to the "root-modal" div. Due to this issue, the
    portal has been deleted and "CreateProjectForm" is now a div component
    instead of a modal.
    
    The same styling as the one in the main div of the "MainDisplay"
    component has been added to "CreateProjectForm". This accomplishes that
    the positioning of both components in the display is exactly the same.
    
    Finally, the "input" and "label" styles have been stored within two
    variables which are used in the corresponding elements, to avoid
    repetition.

--- src/assets/components/CreateProjectForm.jsx
+++ src/assets/components/CreateProjectForm.jsx
@@ -1,5 +1,7 @@
-import { createPortal } from "react-dom";
 import { useRef } from "react";
+
+const labelStyle = "font-bold text-stone-500";
+const inputStyle = "bg-stone-300 border-b-2 border-b-stone-400 rounded-sm";
 
 export default function CreateProjectForm({ onClose, onCreateProject }) {
 
@@ -16,26 +18,26 @@ export default function CreateProjectForm({ onClose, onCreateProject }) {
         onCreateProject(submittedProject);
     }
 
-    return createPortal(
-        <dialog className="dialog w-full" open>
-            <form method="dialog" className="flex flex-col gap-5" onSubmit={handleSubmit}>
+    return (
+        <div className="w-full pt-40 flex flex-col items-center gap-5">
+            <form className="flex flex-col gap-5 w-[700px]" onSubmit={handleSubmit}>
                 <div className="flex justify-end">
                     <button type="button" onClick={onClose} className="rounded-md py-2 px-4">Cancel</button>
                     <button type="submit" className="bg-stone-900 text-white rounded-md py-2 px-6">Save</button>
                 </div>
                 <div className="flex flex-col">
-                    <label className="font-bold text-stone-500" htmlFor="title">TITLE</label>
-                    <input ref={projectTitle} className="bg-stone-300 border-b-2 border-b-stone-400 rounded-sm" id="title" type="text" required/>
+                    <label className={labelStyle} htmlFor="title">TITLE</label>
+                    <input ref={projectTitle} className={inputStyle} id="title" type="text" required/>
                 </div>
                 <div className="flex flex-col">
-                    <label className="font-bold text-stone-500" htmlFor="description">DESCRIPTION</label>
-                    <textarea ref={projectDescription} className="bg-stone-300 border-b-2 border-b-stone-400 rounded-sm" name="" id="description" cols="50" rows="3" required></textarea>
+                    <label className={labelStyle} htmlFor="description">DESCRIPTION</label>
+                    <textarea ref={projectDescription} className={inputStyle} id="description" cols="50" rows="3" required></textarea>
                 </div>
                 <div className="flex flex-col">
-                    <label className="font-bold text-stone-500" htmlFor="due-date">DUE DATE</label>
-                    <input ref={projectDueDate} className="bg-stone-300 border-b-2 border-b-stone-400 rounded-sm" id="due-date" type="date" required/>
+                    <label className={labelStyle} htmlFor="due-date">DUE DATE</label>
+                    <input ref={projectDueDate} className={inputStyle} id="due-date" type="date" required/>
                 </div>
             </form>
-        </dialog>, document.getElementById("modal-root")
+        </div>
     );
 }
